abb88f9197b1b91f364dc510ba0ce4067611c376
Jessice <jessicesun@gmail.com>
2022-05-26 11:33:52 +0800

    更新

--- method/Put.php
+++ method/Put.php
@@ -23,33 +23,33 @@ class Put extends Dav
              * 数据流复制完成后,调用接口
              * 否则回出现文件上传内容为空,文件类型为.txt 情况
              */
-            if($res==true){
+            // if($res==true){
 
-                #Webdav服务端文件绝对路径
-                $Server_Path = WebInfo::$HangingDirectory.$_SERVER['PATH_INFO']; //例如:D:\phpstudy_pro\WWW\gitlab\php_webdav/public/999/a2.txt
+            //     #Webdav服务端文件绝对路径
+            //     $Server_Path = WebInfo::$HangingDirectory.$_SERVER['PATH_INFO']; //例如:D:\phpstudy_pro\WWW\gitlab\php_webdav/public/999/a2.txt
     
-                if(is_file($Server_Path)){
-                    $post_data = [
-                        // "Path"  => "/119a.png",
-                        "Path"  => $_SERVER['PATH_INFO'],
-                        "IsDir" => 0
-                    ];
-                    $file = $Server_Path;
+            //     if(is_file($Server_Path)){
+            //         $post_data = [
+            //             // "Path"  => "/119a.png",
+            //             "Path"  => $_SERVER['PATH_INFO'],
+            //             "IsDir" => 0
+            //         ];
+            //         $file = $Server_Path;
 
-                    // php 5.5+ 需要使用 curl_file_create 方法
-                    if (function_exists('curl_file_create')) {
-                        $post_data['File'] = curl_file_create($file);
-                    } else {
-                        $post_data['File'] = '@' . $file;
-                    }
+            //         // php 5.5+ 需要使用 curl_file_create 方法
+            //         if (function_exists('curl_file_create')) {
+            //             $post_data['File'] = curl_file_create($file);
+            //         } else {
+            //             $post_data['File'] = '@' . $file;
+            //         }
                     
-                    $data = $this->DavCurl->curl_upload_file(WebInfo::$ApiUrl."Index/addFileOrDir", $post_data);
-                    $this->sqllog('文件上传',json_encode([
-                        'path'=> $post_data['Path'],
-                        'data' => json_decode($data,true),
-                    ],JSON_UNESCAPED_UNICODE));
-                }
-            }
+            //         $data = $this->DavCurl->curl_upload_file(WebInfo::$ApiUrl."Index/addFileOrDir", $post_data);
+            //         $this->sqllog('文件上传',json_encode([
+            //             'path'=> $post_data['Path'],
+            //             'data' => json_decode($data,true),
+            //         ],JSON_UNESCAPED_UNICODE));
+            //     }
+            // }
 
         }catch (Throwable $throwable){
             $this->DavXml->response_http_code(503);
